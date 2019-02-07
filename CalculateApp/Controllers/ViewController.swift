5a9aeda73873f811a1d4cd4625b5e88b5e5b2f1e
Avisa <avisa@avisas-imac.local>
2019-02-07 21:25:49 +1100

    ViewController is ready .

--- CalculateApp/Controllers/ViewController.swift
+++ CalculateApp/Controllers/ViewController.swift
@@ -9,12 +9,34 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var displayLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
+    
+    
+    
+    
 
-
+    @IBAction func calcButtonPressed(_ sender: UIButton) {
+        //What should happen when non-number button pressed
+        
+    }
+    
+    
+    
+    
+    
+    
+    
+    @IBAction func numButtonPressed(_ sender: UIButton) {
+        //what should happen when a number is entered into keypad
+        
+        
+    }
+    
 }
 
